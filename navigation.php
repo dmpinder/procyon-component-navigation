2a550d060c9fc8c4199323c761100cb9444bc413
Matt Radford <matt@10degrees.uk>
2017-10-20 17:53:36 +0100

    Semver. Some plugin descriptions changes since move to Github.

--- navigation.php
+++ navigation.php
@@ -1,13 +1,13 @@
 <?php
 /*
-  Plugin Name: Procyon Component: Navigation
-  Plugin URI: https://bitbucket.org/dmpinder/procyon-component-navigation
-  Description: A drop-in PHP component to create a WCAG-compliant logo. Based on the excellent 10up component library.
-  Author: Darren Pinder, 10up
-  Version: 1.0
-  Author URI: https://vatu.co.uk
-  GitHub Plugin URI: https://bitbucket.org/dmpinder/procyon-component-navigation
-  GitHub Branch:     master
+  Plugin Name:          Procyon Component: Navigation
+  Plugin URI:           https://github.com/dmpinder/procyon-component-navigation
+  Description:          A drop-in PHP component to create a WCAG-compliant logo. Based on the excellent 10up component library.
+  Author:               Darren Pinder, 10up
+  Version:              1.0.0
+  Author URI:           https://vatu.co.uk
+  GitHub Plugin URI:    https://github.com/dmpinder/procyon-component-navigation
+  GitHub Branch:        master
  */
 
 function procyon_component_navigation() { 
@@ -15,14 +15,13 @@ function procyon_component_navigation() {
     wp_enqueue_style( 'procyon_component_navigation_css', plugin_dir_url( __FILE__ ) . 'css/navigation.css', '', '1.0', '' );
     wp_enqueue_script( 'procyon_component_navigation_js', plugin_dir_url( __FILE__ ) . 'js/navigation.min.js', '', '1.0', '' );
     wp_enqueue_script( 'procyon_component_navigation_nav', plugin_dir_url( __FILE__ ) . 'js/nav-settings.js', '', '1.0', '' );
+    
     ?>
 
     <nav class="site-navigation" role="navigation" itemscope="itemscope" itemtype="http://schema.org/SiteNavigationElement">
 
         <a href="#primary-menu" id="js-menu-toggle" class="site-menu-toggle">
-            <span class="screen-reader-text">
-                <?php esc_html_e( 'Primary Menu', 'tenup' ); ?>
-            </span>
+            <span class="screen-reader-text"><?php esc_html_e( 'Primary Menu', 'tenup' ); ?></span>
             <span aria-hidden="true">&#9776;</span>
         </a>
 
@@ -33,6 +32,7 @@ function procyon_component_navigation() {
 				'menu'           => 'main-menu',
             ) );
         ?>
+
     </nav>
 
 <?php } ?>
